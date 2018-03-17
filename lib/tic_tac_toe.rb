ec872f94ad8db61d5829c995ecb95baae09b7ee4
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-17 22:12:15 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,7 +15,7 @@ class TicTacToe
     puts " #{board[3]} | #{board[4]} | #{board[5]} "
     puts "-----------"
     puts " #{board[6]} | #{board[7]} | #{board[8]} "
-  end  
+  end
 
   def input_to_index(user_input)
     user_input.to_i - 1
@@ -70,5 +70,20 @@ class TicTacToe
       "O"
     end
   end
-  
-end  
+
+  def won?(board)
+    WIN_COMBINATIONS.each do |win_combination|
+      win_index_1 = win_combination[0]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
+      position_1 = board[win_index_1]
+      position_2 = board[win_index_2]
+      position_3 = board[win_index_3]
+      if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return win_combination
+      end
+    end
+    false
+  end
+
+end
