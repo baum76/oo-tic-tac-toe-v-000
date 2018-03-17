8ba5582c95ae3ef447d15c4a4eafaa73b3024982
Matt Baumgarten <mattybaumgarten@gmail.com>
2018-03-17 22:11:43 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,12 +3,72 @@ class TicTacToe
   [0, 1, 2], [3, 4, 5], [6, 7, 8], #horizontal
   [0, 3, 6], [1, 4, 7], [2, 5, 8], #vertical
   [0, 4, 8], [2, 4, 6] #diagonal
-]
+  ]
 
   def initialize(board)
     @board = board
   end
 
+  def display_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end  
 
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
 
-end
+  def move(board, index, value)
+    board[index] = value
+  end
+
+  def position_taken?(board, index)
+    if board[index] == " " || board[index] == ""
+      false
+    elsif board[index] == nil
+      false
+    else
+      true
+    end
+  end
+
+  def valid_move?(board, index)
+    if !position_taken?(board, index) && index.between?(0, 8)
+      true
+    end
+  end
+
+  def turn(board)
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(board, index)
+      move(board, index, current_player(board))
+      display_board(board)
+    else
+      turn(board)
+    end
+  end
+
+  def turn_count(board)
+    counter = 0
+    board.each do |space|
+      if space == "X" || space == "O"
+        counter += 1
+      end
+    end
+    counter
+  end
+
+  def current_player(board)
+    if turn_count(board).even?
+      "X"
+    elsif turn_count(board).odd?
+      "O"
+    end
+  end
+  
+end  
